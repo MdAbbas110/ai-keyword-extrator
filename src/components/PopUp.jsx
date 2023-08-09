e13d986b92afbef8480698a46f102a2e40938fa5
Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2023-08-09 23:40:36 +0530

    Create PopUp.jsx
    new component to popUp with the kleys

---
+++ src/components/PopUp.jsx
@@ -0,0 +1,15 @@
+import react from 'react'
+
+const PopUp = () => {
+
+  const [update, setUpdate] = useEffect(flase)
+
+  return (
+    <>
+      <input  />
+      <button>{close}</button>
+    </>
+
+    }
+
+export default PopUp
